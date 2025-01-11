71a126d1967aa3ad0dff512cad320e49353cd665
Kishan Vishvakarma <124333038+Kishansanjay@users.noreply.github.com>
2025-01-11 11:55:57 +0000

    first programe done

---
+++ javascript/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 14455
+let accountEmail = "kishan@gmail.com"
+var accountPassword = "971455834"
+accountCity = "silvassa"
+
+let accountState;
+
+// accountId = 123
+accountEmail = "ksihan2@gmail.com"
+accountPassword = "1245"
+accountCity = "dadra"
+
+// console.log(accountId);
+
+/*
+preffer not to use var 
+because we will not able to use scope in var
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
